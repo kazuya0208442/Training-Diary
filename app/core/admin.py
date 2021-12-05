601c7911e95154d1d4ea3f55ff0e0008da9f8f62
kazuya0208442 <kazuya@DESKTOP-SO905U3.localdomain>
2021-12-05 09:57:04 +0900

    first comit

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from core.models import Sample
+from .models import TodoModel     #エラー出たから分けたけど、関係ないかも
 
 # Register your models here.
 admin.site.register(Sample)
+# admin.site.register(TodoModel)
+admin.site.register(TodoModel)
--- app/core/models.py
+++ app/core/models.py
@@ -3,3 +3,17 @@ from django.db import models
 # Create your models here.
 class Sample(models.Model):
     attachment = models.FileField()
+
+CHOICE = (('danger', 'high'), ('warning', 'normal'), ('primary', 'low'))
+
+class TodoModel(models.Model):
+    title = models.CharField(max_length=100)
+    memo = models.TextField()
+    priority = models.CharField(
+        max_length = 50,
+        choices = CHOICE
+        )
+    duedate = models.DateField()
+
+    def __str__(self):
+        return self.title
