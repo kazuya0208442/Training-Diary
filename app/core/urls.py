99cb10dfabb7e397740c000e9073224b3e926625
kazuya0208442 <inomata.kazuya.417@gmail.com>
2022-01-19 19:01:59 +0900

    delete_trash

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,14 +1,8 @@
 from django.urls import path, include
-from .views import CardUpdate, Staff_detail, StaffUpdate, Target_detail, TargetUpdate, TodoList, TodoDetail, TodoCreate, TodoDelete, TodoUpdate, Home, Card_detail, Week_detail, WeekUpdate
+from .views import CardUpdate, Staff_detail, StaffUpdate, Target_detail, Home, Card_detail, Week_detail, WeekUpdate, TargetUpdate
 
 
 urlpatterns = [
-    path('list', TodoList.as_view(), name='list'),
-    path('detail/<int:pk>', TodoDetail.as_view(), name='detail'),
-    path('create/', TodoCreate.as_view(), name='create'),
-    path('delete/<int:pk>', TodoDelete.as_view(), name='delete'),
-    path('update/<int:pk>', TodoUpdate.as_view(), name='update'),
-    
     path('', Home.as_view(), name='home'),
     path('t_detail/<int:pk>', Target_detail.as_view(), name='t_detail'),
     path('c_detail/<int:pk>', Card_detail.as_view(), name='c_detail'),
--- app/core/views.py
+++ app/core/views.py
@@ -1,40 +1,12 @@
 from django.db.models import fields
 # from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, DeleteView, UpdateView
-from .models import Staff, Target, TodoModel, Training, Week
+from .models import Staff, Target, Training, Week
 from django.urls import reverse_lazy
 from .change_link import change_link
 from .day_computed import day_computed
 
-
-
 # Create your views here.
-
-class TodoList(ListView):
-    template_name = 'list.html'
-    model = TodoModel
-
-class TodoDetail(DetailView):
-    template_name = 'detail.html'
-    model = TodoModel
-
-class TodoCreate(CreateView):
-    template_name = 'create.html'
-    model = TodoModel
-    fields = ('title', 'memo', 'priority', 'duedate')
-    success_url = reverse_lazy('list')
-
-class TodoDelete(DeleteView):
-    template_name = 'delete.html'
-    model = TodoModel
-    success_url = reverse_lazy('list')
-
-class TodoUpdate(UpdateView):
-    template_name = 'update.html'
-    model = TodoModel
-    fields = ('title', 'memo', 'priority', 'duedate')
-    success_url = reverse_lazy('list')
-
 
 
 class Home(ListView):
