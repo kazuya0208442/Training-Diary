84b8730784b73d60f4e84f0dc3137a7fab6233d6
kazuya0208442 <inomata.kazuya.417@gmail.com>
2022-01-20 13:01:04 +0900

    exec test

--- app/core/tests.py
+++ app/core/tests.py
@@ -1,8 +1,15 @@
 from django.test import TestCase
-
+from .change_link import change_link
+from .day_computed import day_computed
 # Create your tests here.
 
 class TestCi(TestCase):
 
     def test(self):
         self.assertEqual(1, 1)
+    
+    def test_daycomputed(self):
+        self.assertEqual(day_computed('2022-01-21'), 1)
+
+    def test_change_link(self):
+        self.assertEqual(change_link(''), )
