e06220773b90fd3ce6954d75eeaa8eefe78ad35d
kazuya0208442 <inomata.kazuya.417@gmail.com>
2022-01-20 15:13:05 +0900

    add_test_#117

--- app/core/tests.py
+++ app/core/tests.py
@@ -1,15 +1,15 @@
 from django.test import TestCase
 from .change_link import change_link
 from .day_computed import day_computed
-# Create your tests here.
+import datetime
 
+# Create your tests here.
 class TestCi(TestCase):
 
-    def test(self):
-        self.assertEqual(1, 1)
-    
     def test_daycomputed(self):
-        self.assertEqual(day_computed('2022-01-21'), 1)
+        self.assertEqual(day_computed(datetime.date.today()), 1)
 
     def test_change_link(self):
-        self.assertEqual(change_link(''), )
+        string = '<iframe width="560" height="315" src="https://www.youtube.com/embed/eZA0AnSdbBI" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        ans = '<iframe class="content" width="560" height="315" src="https://www.youtube.com/embed/eZA0AnSdbBI?autoplay=1&mute=1&start=25" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>'
+        self.assertEqual(change_link(string, 25), ans)
